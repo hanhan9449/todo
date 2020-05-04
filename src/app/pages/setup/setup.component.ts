46e89bb4354f53b24fca81698af18794bb6511f1
Han Han <haannn@qq.com>
2020-05-04 13:55:15 +0800

    feat(release): login interface html and ts

--- src/app/pages/setup/setup.component.ts
+++ src/app/pages/setup/setup.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../../services/local-storage/local-storage.service';
+import {
+  INIT_FLAG,
+  START_USING_DATE,
+  USERNAME,
+} from '../../services/local-storage/local-storage.namespace';
+
+function getTodayTime(): number {
+  return Date.now();
+}
 
 @Component({
   selector: 'app-setup',
@@ -6,7 +16,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setup.component.less'],
 })
 export class SetupComponent implements OnInit {
-  constructor() {}
+  username: string;
+  constructor(private store: LocalStorageService) {}
 
   ngOnInit(): void {}
+  completeSetup(): void {
+    this.store.set(INIT_FLAG, true);
+    this.store.set(START_USING_DATE, getTodayTime());
+    this.store.set(USERNAME, this.username);
+  }
 }
--- src/app/pages/setup/setup.module.ts
+++ src/app/pages/setup/setup.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupComponent } from './setup.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SetupComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgZorroAntdModule, FormsModule],
 })
 export class SetupModule {}
