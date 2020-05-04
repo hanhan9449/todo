6eee7ff9af99c2e675010d5da5c0f9c4744b9e22
Han Han <haannn@qq.com>
2020-05-04 13:18:44 +0800

    feat(release): initial setup.component

--- src/app/pages/setup/setup.module.ts
+++ src/app/pages/setup/setup.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SetupComponent } from './setup.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SetupComponent],
   imports: [
     CommonModule
   ]
