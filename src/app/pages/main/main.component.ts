c2a974f5457f2624b7f7e9aed61c5a92f27d0868
Han Han <haannn@qq.com>
2020-05-04 14:49:57 +0800

    style(main): reformat by prettier

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.less']
+  styleUrls: ['./main.component.less'],
 })
 export class MainComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
